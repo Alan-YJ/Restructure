c68227a9d19c77ad0a843a27bfb7468136189c09
老妖怪 <oncwnuKMqAhaF_4PRTR7LJ0kr0OU@git.weixin.qq.com>
2021-02-28 23:16:09 +0800

    chapter1.4

--- js/index.js
+++ js/index.js
@@ -38,66 +38,84 @@ let invoices = [
 
 //打印账单详情
 function statement(invoice, plays){
-    //总金额
-    let totalAmount = 0;
+
+    function amountFor(aPerformance){
+        let result = 0;
+    
+        //1.如果表演类型为悲剧，起价40000，如果观看人数大于30人，每多出一人收1000块
+        //2.如果表演类型为喜剧，起价30000，每个座位都收取300的座位费，如果观看人数大于20人，则多收10000的费用以及每个超出的座位收500块
+        switch(playFor(aPerformance).type){
+            case "tragedy":
+                result = 40000;
+                if(aPerformance.audience > 30){
+                    result += 1000 * (aPerformance.audience - 30)
+                }
+                break;
+            case "comedy":
+                result = 30000
+                if(aPerformance.audience > 20){
+                    result += 10000 + 500*(aPerformance.audience -20) 
+                }
+                result += 300*aPerformance.audience
+                break;
+            default:
+                throw new Error(`unknown type:${playFor(aPerformance).type}`)
+        }
+    
+        return result
+    }    
+
+    function playFor(aPerformance){
+        return plays[aPerformance.playID]
+    }
+
     //积分
-    let volumeCredits = 0;
+    function volumeCreditsFor(aPerformance) {
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0)
+        if("comedy" === playFor(aPerformance).type){
+            result += Math.floor(aPerformance.audience/5)
+        }
+        return result
+    }
+
+    function usd(aNumber){
+        return new Intl.NumberFormat("en-US", {
+            style: "currency",  //带货币符号
+            currency: "USD",    //币种
+            minimumFractionDigits: 2    //小数点位数
+        }).format(aNumber/100)
+    }
+
+    //总积分信息
+    function totalVolumeCredits(){
+        let result = 0;
+        for(let perf of invoice.performances){
+            result += volumeCreditsFor(perf)
+        }
+        return result
+    }
+
+    //总金额
+    function appleSauce(){
+        let result = 0;
+        for(let perf of invoice.performances){
+            result += amountFor(perf)
+        }
+        return result
+    }
+
     //打印信息
     let result = `Statement for ${invoice.customer}\n`;
-    let formatConfig = {
-        style: "currency",  //带货币符号
-        currency: "USD",    //币种
-        minimumFractionDigits: 2    //小数点位数
-    }
-    //Intl.NumberFormat根据配置转换成指定货币类型字符串,使用方式format(value)
-    const format = new Intl.NumberFormat("en-US", formatConfig).format
 
-    //循环读取客户的每一条账单
     for(let perf of invoice.performances){
-        //找到表演者信息
-        const play = plays[perf.playID];
-
-        let thisAmount = amountFor(perf, play)
-        //计算积分信息
-        volumeCredits += Math.max(perf.audience - 30, 0)
-        if("comedy" === play.type){
-            volumeCredits += Math.floor(perf.audience/5)
-        }
-
-        result += `${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`
-        totalAmount += thisAmount
+        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
     }
 
-    result += `Amount owed is ${format(totalAmount/100)}\n`
-    result += `You earned ${volumeCredits} credits\n`
+    result += `Amount owed is ${usd(appleSauce())}\n`
+    result += `You earned ${totalVolumeCredits()} credits\n`
 
     return result
-}
-
-function amountFor(perf, play){
-    let thisAmount = 0;
-
-    //1.如果表演类型为悲剧，起价40000，如果观看人数大于30人，每多出一人收1000块
-    //2.如果表演类型为喜剧，起价30000，每个座位都收取300的座位费，如果观看人数大于20人，则多收10000的费用以及每个超出的座位收500块
-    switch(play.type){
-        case "tragedy":
-            thisAmount = 40000;
-            if(perf.audience > 30){
-                thisAmount += 1000 * (perf.audience - 30)
-            }
-            break;
-        case "comedy":
-            thisAmount = 30000
-            if(perf.audience > 20){
-                thisAmount += 10000 + 500*(perf.audience -20) 
-            }
-            thisAmount += 300*perf.audience
-            break;
-        default:
-            throw new Error(`unknown type:${play.type}`)
-    }
-
-    return thisAmount
 }
 
 console.info(statement(invoices[0],plays))
