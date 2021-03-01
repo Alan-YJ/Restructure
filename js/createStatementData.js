27cc57a9dbe4835694e524a884318e6ce179495e
yijing <yijing5893@dingtalk.com>
2021-03-01 18:30:41 +0800

    11..66import

---
+++ js/createStatementData.js
@@ -0,0 +1,67 @@
+
+export default function createStatementData(invoice){
+    let statementData = {}
+    statementData.customer = invoice.customer
+    statementData.performances = invoice.performances.map(setPerformance)
+    statementData.totalAmount = totalAmount(statementData)
+    statementData.totalVolumeCredits = totalVolumeCredits(statementData)
+    return statementData
+}
+
+function setPerformance(aPerformance){
+    let result = Object.assign({}, aPerformance)
+    result.play = playFor(result)
+    result.amount = amountFor(result)
+    result.volumeCredits = volumeCreditsFor(result)
+    return result
+}
+
+function playFor(aPerformance){
+    return plays[aPerformance.playID]
+}
+
+function amountFor(aPerformance){
+    let result = 0;
+    //1.如果表演类型为悲剧，起价40000，如果观看人数大于30人，每多出一人收1000块
+    //2.如果表演类型为喜剧，起价30000，每个座位都收取300的座位费，如果观看人数大于20人，则多收10000的费用以及每个超出的座位收500块
+    switch(aPerformance.play.type){
+        case "tragedy":
+            result = 40000;
+            if(aPerformance.audience > 30){
+                result += 1000 * (aPerformance.audience - 30)
+            }
+            break;
+        case "comedy":
+            result = 30000
+            if(aPerformance.audience > 20){
+                result += 10000 + 500*(aPerformance.audience -20) 
+            }
+            result += 300*aPerformance.audience
+            break;
+        default:
+            throw new Error(`unknown type:${aPerformance.play.type}`)
+    }
+
+    return result
+}   
+
+//积分
+function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0)
+    if("comedy" === aPerformance.play.type){
+        result += Math.floor(aPerformance.audience/5)
+    }
+    return result
+}
+
+//总积分信息
+function totalVolumeCredits(data){
+    return data.performances.reduce((total, p)=> total + p.volumeCredits, 0)
+}
+
+//总金额
+function totalAmount(data){
+    return data.performances.reduce((total, p)=> total + p.amount, 0)
+}
+
--- js/index.js
+++ js/index.js
@@ -36,84 +36,29 @@ let invoices = [
     }
 ]
 
+function statement(invoice){
+    return renderPlainText(createStatementData(invoice))
+}
+
+function usd(aNumber){
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",  //带货币符号
+        currency: "USD",    //币种
+        minimumFractionDigits: 2    //小数点位数
+    }).format(aNumber/100)
+}
+
 //打印账单详情
-function statement(invoice, plays){
-
-    function amountFor(aPerformance){
-        let result = 0;
-    
-        //1.如果表演类型为悲剧，起价40000，如果观看人数大于30人，每多出一人收1000块
-        //2.如果表演类型为喜剧，起价30000，每个座位都收取300的座位费，如果观看人数大于20人，则多收10000的费用以及每个超出的座位收500块
-        switch(playFor(aPerformance).type){
-            case "tragedy":
-                result = 40000;
-                if(aPerformance.audience > 30){
-                    result += 1000 * (aPerformance.audience - 30)
-                }
-                break;
-            case "comedy":
-                result = 30000
-                if(aPerformance.audience > 20){
-                    result += 10000 + 500*(aPerformance.audience -20) 
-                }
-                result += 300*aPerformance.audience
-                break;
-            default:
-                throw new Error(`unknown type:${playFor(aPerformance).type}`)
-        }
-    
-        return result
-    }    
-
-    function playFor(aPerformance){
-        return plays[aPerformance.playID]
-    }
-
-    //积分
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0)
-        if("comedy" === playFor(aPerformance).type){
-            result += Math.floor(aPerformance.audience/5)
-        }
-        return result
-    }
-
-    function usd(aNumber){
-        return new Intl.NumberFormat("en-US", {
-            style: "currency",  //带货币符号
-            currency: "USD",    //币种
-            minimumFractionDigits: 2    //小数点位数
-        }).format(aNumber/100)
-    }
-
-    //总积分信息
-    function totalVolumeCredits(){
-        let result = 0;
-        for(let perf of invoice.performances){
-            result += volumeCreditsFor(perf)
-        }
-        return result
-    }
-
-    //总金额
-    function appleSauce(){
-        let result = 0;
-        for(let perf of invoice.performances){
-            result += amountFor(perf)
-        }
-        return result
-    }
-
+function renderPlainText(data){
     //打印信息
-    let result = `Statement for ${invoice.customer}\n`;
+    let result = `Statement for ${data.customer}\n`;
 
-    for(let perf of invoice.performances){
-        result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`
+    for(let perf of data.performances){
+        result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`
     }
 
-    result += `Amount owed is ${usd(appleSauce())}\n`
-    result += `You earned ${totalVolumeCredits()} credits\n`
+    result += `Amount owed is ${usd(data.totalAmount)}\n`
+    result += `You earned ${data.totalVolumeCredits} credits\n`
 
     return result
 }
